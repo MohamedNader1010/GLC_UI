a241a7ce0421c7455cc2617093a0da2ae0649d5f
Mohamed Mahmoud El-Kallaf <112264148+kallaf10@users.noreply.github.com>
2022-12-02 23:36:45 +0200

    Log in created (#4)
    Log in created

--- src/app/modules/authuntecation/authentication-routing.module.ts
+++ src/app/modules/authuntecation/authentication-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './compenent/login/login.component';
 import { ForgetPasswordComponent} from './forget-password/forget-password.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { SignUpComponent } from './sign-up/sign-up.component'; 
@@ -8,7 +9,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 const routes: Routes = [
   {path : 'signup', component : SignUpComponent},
   {path : 'forget', component : ForgetPasswordComponent},
-  {path : 'reset', component : ResetPasswordComponent}
+  {path : 'reset', component : ResetPasswordComponent},
+  {path:'login',component:LoginComponent},
 ];
   
 @NgModule({
